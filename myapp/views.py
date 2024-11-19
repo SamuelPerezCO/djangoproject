ff066580cd32f19d4a63f6507a43c74617efd443
SamuelPerezCO <sernasamuelperez@gmail.com>
2024-11-19 17:22:56 -0500

    Params

--- myapp/views.py
+++ myapp/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def hello(request):
-    return HttpResponse("<h1>Hello World</h1>")
+def index(request):
+    return HttpResponse("Index page")
+
+def hello(request, username):
+    print(username)
+    return HttpResponse("<h1>Hello %s</h1>" % username)
 
 def about(request):
     return HttpResponse('<h2>About</h2>')
