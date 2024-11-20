9cfbe3b98f95212f39e2c757cf9ae05c48678e34
SamuelPerezCO <sernasamuelperez@gmail.com>
2024-11-20 11:29:42 -0500

    templates

--- myapp/views.py
+++ myapp/views.py
@@ -4,19 +4,28 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    title = 'Django Course!!'
+    return render(request, 'index.html' , {
+        'title':title
+    })
 
 def about(request):
-    return render(request, 'about.html')
+    username = 'fazt'
+    return render(request, 'about.html' , {
+        'username':username
+    })
 
 def hello(request, username):
     print(username)
     return HttpResponse("<h1>Hello %s</h1>" % username)
 
 def projects(request):
-    projects = list(Project.objects.values())
-    return render(request, 'projects.html')
+    # projects = list(Project.objects.values())
+    projects = Project.objects.all()
+    return render(request, 'projects.html',{
+        'projects':projects
+    })
 
 def tasks(request):
     # task = Task.objects.get(title = title)
-    return render(request, 'tasks.html')
+    return render(request, 'tasks.html') 
